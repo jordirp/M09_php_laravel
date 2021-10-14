5a99ac37c4eab29b85358ff3be82e2f0dfe16782
jordirp <jordirodriguez2@iesebre.com>
2021-10-14 19:26:34 +0200

    fin dia 14/10

--- app/index.php
+++ app/index.php
@@ -1,2 +1,11 @@
 <?php
-$greeting = 'Hola mon!';
+//Query string
+//var_dump ($_GET['name']);
+//$name = 'Sergi';
+$name = $_GET['name'];
+$username = $_GET['username'];
+
+$greeting = "Hola $name $username !" ;
+
+//$greeting = 'Hola' . $_GET['name'] . '!' ;
+
