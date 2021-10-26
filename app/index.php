ed8574b4916bb07b8721eae460ecec7d6affe692
jordirp <jordirodriguez2@iesebre.com>
2021-10-26 16:12:10 +0200

    min 34.18 'Commits' Creació de config i refactoritzacio de index

--- app/index.php
+++ app/index.php
@@ -1,35 +1,17 @@
 <?php
 require 'app/helpers.php';
 require 'app/Task.php';
+require 'config.php';
 
-$task = new Task(1,'comprar pa', 'a la panaderia',0);
-var_dump($task);
 //Query string
 //var_dump ($_GET['name']);
 //$name = 'Sergi';
 
-
-$config = [
-    'database' => [
-        'user' =>'debian-sys-maint',
-        'password' =>'ri48e17r9cfJIqGu',
-        'databasetype' =>'mysql',
-        'host' =>'localhost',
-        'name' =>'phplaraveldevs',
-    ]
-];
-
-
-$user = $config ['database']['user'];
-$pass=  $config ['database']['password'];
-$type=  $config ['database']['databasetype'];
-$host=  $config ['database']['host'];
-$dbname=  $config ['database']['phplaraveldevs'];
-$dsn ="$type: host= $host;dbname =$dbname";
-
 try {
-    $dbh = new PDO($dsn,$user,$pass);
-
+    $dbh = new PDO(
+        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+        $config['database']['user'],
+        $config['database']['password']);
 }catch (\Exception $e) {
     echo 'Error de conexió base de dades';
 }
@@ -37,9 +19,6 @@ try {
 $statement = $dbh -> prepare('SELECT * FROM tasks');
 $statement-> execute();
 $task=$statement-> fetch(PDO::FETCH_CLASS,'Tasks');
-
-var_dump($task);
-
 $greeting = greet();
 
 
